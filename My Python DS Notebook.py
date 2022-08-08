901038755efbea5b2b84af6e3db7d519b15203e1
ManuTC1 <110581988+ManuTC1@users.noreply.github.com>
2022-08-08 08:40:34 +0530

    Add files via upload

---
+++ My Python DS Notebook.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# Name: **MANU** <br>
+# Desired Occupation: Data Scientist
+
+# *I am interested in data science because iam a data enthuiast and my past 7-10 years of professional work has been in and around data viz data loading as part of ERP implementation and metadata management as part of Data Governance.*
+
+# ### My code in the next cell is suppose to add the 2 numeric values and print the output
+
+# In[6]:
+
+
+Result = 100 + 55
+print (Result)
+
+
+# <blockquote> This is to showcase the blocked text in Notebook </blockquote> <br>
+
+# #### The below example is to showcase the numbered list
+# <ol>
+# <li>Data Engineer</li>
+# <li>Data Analyst</li>
+# <li>Data Scientist</li>
+# </ol>
+
+# #### Below is to showcase the Table
+# |Name|Address|Salary| 
+# |-----|-------|------| 
+# |Jack|USA|4000| 
+# |Jones|Canada|5000|
+
+# #### The below example is to showcase the buletted list
+# - Data Engineer
+# - Data Analyst
+# - Data Scientist
+
+# ~~ This is to showcase the Strikethrough text in Notebook ~~
+
+# In[ ]:
+
+
+
+
